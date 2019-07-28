49fce547e0d3ddc8788f612a7f0431254c4032b8
BaaDNwZ <badnwz.ad@gmail.com>
2019-07-29 00:54:49 +0300

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,37 +5,3 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
-
-# This script will be executed in post-fs-data mode
-#Google analytics disable commands
-pm disable com.google.android.gms/.update.SystemUpdateService
-pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
-pm disable com.google.android.gsf/.update.SystemUpdateService
-pm disable com.android.vending/com.google.android.gms.measurement.AppMeasurementReceiver
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdInternalReceiver
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdReceiver
-pm disable com.google.android.gms/.update.SystemUpdateActivity
-pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
-pm disable com.google.android.gms/.update.SystemUpdateService$Receiver
-pm disable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsReceiver
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerInternalReceiver
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerReceiver
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementReceiver
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementService
-pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementReceiver
-pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementService
-pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
-pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
-pm disable com.google.android.gsf/.update.SystemUpdateActivity
-pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity
-pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver
-pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
