ef7dd2880099e9250d5fc4e6b3ceb3016413e908
BaaDNwZ <badnwz.ad@gmail.com>
2019-07-30 19:59:32 +0300

    Fixes

--- common/service.sh
+++ common/service.sh
@@ -14,37 +14,132 @@ settings put global window_animation_scale 0.8
 settings put global transition_animation_scale 0.8
 settings put global animator_duration_scale 0.8
 
-sleep 150
+sleep 140
 #Google analytics disable commands
 pm disable com.google.android.gms/.update.SystemUpdateService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
+sleep 1
 pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
+sleep 1
 pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
+sleep 1
 pm disable com.google.android.gsf/.update.SystemUpdateService
+sleep 1
 pm disable com.android.vending/com.google.android.gms.measurement.AppMeasurementReceiver
+sleep 1
 pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdInternalReceiver
+sleep 1
 pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdReceiver
+sleep 1
 pm disable com.google.android.gms/.update.SystemUpdateActivity
+sleep 1
 pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+sleep 1
 pm disable com.google.android.gms/.update.SystemUpdateService$Receiver
+sleep 1
 pm disable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerInternalReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementReceiver
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementService
+sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
+sleep 1
 pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
+sleep 1
 pm disable com.google.android.gsf/.update.SystemUpdateActivity
+sleep 1
 pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity
+sleep 1
 pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver
+sleep 1
 pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+sleep 1
 
-
+sleep 90
+#Google analytics disable commands - again, just in case the first one didn't work for all lines
+pm disable com.google.android.gms/.update.SystemUpdateService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
+sleep 1
+pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
+sleep 1
+pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
+sleep 1
+pm disable com.google.android.gsf/.update.SystemUpdateService
+sleep 1
+pm disable com.android.vending/com.google.android.gms.measurement.AppMeasurementReceiver
+sleep 1
+pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdInternalReceiver
+sleep 1
+pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdReceiver
+sleep 1
+pm disable com.google.android.gms/.update.SystemUpdateActivity
+sleep 1
+pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+sleep 1
+pm disable com.google.android.gms/.update.SystemUpdateService$Receiver
+sleep 1
+pm disable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerInternalReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementReceiver
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementService
+sleep 1
+pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
+sleep 1
+pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
+sleep 1
+pm disable com.google.android.gsf/.update.SystemUpdateActivity
+sleep 1
+pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity
+sleep 1
+pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver
+sleep 1
+pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+sleep 1
