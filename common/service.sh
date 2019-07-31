32ab69c611ce97da3568ab5cdfa6c7a71bbd758a
BaaDNwZ <badnwz.ad@gmail.com>
2019-08-01 00:10:47 +0300

    Important changes
    Clean & updated service disabling list.

--- common/service.sh
+++ common/service.sh
@@ -22,128 +22,12 @@ settings put global window_animation_scale 0.8
 settings put global transition_animation_scale 0.8
 settings put global animator_duration_scale 0.8
 
-sleep 30
-#Google analytics disable commands
-##Recievers
-pm disable com.android.vending/com.google.android.gms.measurement.AppMeasurementReceiver
-sleep 1
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdInternalReceiver
-sleep 1
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdReceiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$Receiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerInternalReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
-sleep 1
-
-##Services
-pm disable com.google.android.gms/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.common.stats.GmsCoreStatsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.clearcut.debug.ClearcutDebugDumpService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.common.stats.GmsCoreStatsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.clearcut.debug.ClearcutDebugDumpService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
-sleep 1
-
-##Activities
-pm disable com.google.android.gms/.update.SystemUpdateActivity
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateActivity
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity
-sleep 1
-
-
-
 sleep 90
-#Google analytics disable commands - again, just in case the first one didn't work for all lines
-##Recievers
+#Google analytics disable commands
+#Recievers
 pm disable com.android.vending/com.google.android.gms.measurement.AppMeasurementReceiver
 sleep 1
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdInternalReceiver
-sleep 1
-pm disable com.android.vending/com.google.firebase.iid.FirebaseInstanceIdReceiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$Receiver
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
-sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerInternalReceiver
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.nts.SchedulerReceiver
 sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
 sleep 1
@@ -153,16 +37,8 @@ pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeas
 sleep 1
 pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
 sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
-sleep 1
 
 ##Services
-pm disable com.google.android.gms/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
 sleep 1
 pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
@@ -174,40 +50,6 @@ sleep 1
 pm disable com.google.android.gms/com.google.android.gms.clearcut.debug.ClearcutDebugDumpService
 sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/.update.SystemUpdateService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsTaskService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.common.stats.GmsCoreStatsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.clearcut.debug.ClearcutDebugDumpService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsJobService
-sleep 1
-pm disable com.google.android.syncadapters.contacts/com.google.android.gms.analytics.AnalyticsService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.chimera.PersistentIntentOperationService
-sleep 1
-pm disable com.google.android.gms/com.google.android.gms.gcm.HeartbeatAlarm$ConnectionInfoPersistService
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateService
 sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementService
 sleep 1
@@ -215,11 +57,5 @@ pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeas
 sleep 1
 pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
 sleep 1
+pm disable com.google.android.gms/com.google.android.gms.usagereporting.service.UsageReportingService
 
-##Activities
-pm disable com.google.android.gms/.update.SystemUpdateActivity
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdateActivity
-sleep 1
-pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity
-sleep 1
